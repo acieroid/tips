5a5e5d6566916c6adad0bea3125b24326e9830c2
Quentin Stievenart <quentin.stievenart@gmail.com>
2013-09-15 14:58:19 +0200

    Implement registration

--- tips.ml
+++ tips.ml
@@ -1,10 +1,7 @@
-(* packages needed: ocsigenserver, eliom.server
-*)
 open Eliom_parameter
 open Eliom_content.Html5.D
 
 (* TODO:
-  - connect_box ()
   - Tips.display (tips list)
 *)
 
@@ -31,41 +28,45 @@ let footer =
      (* pcdata " - " *)
      (* TODO: link to source code *)]
 
-let main_service_body _ _ =
+let home_body _ _ =
+  Lwt.return (p [pcdata "TODO: display tips"])
+  (* Tips.display (Database.get_n_most_recent_tips 5) *)
+
+let todo_body _ _ =
+  Lwt.return
+    (p [pcdata "TODO"])
+
+let page f a b =
   lwt menu = menu () in
-  lwt tips = Lwt.return (p [pcdata "TODO: display tips"])
-  (* Tips.display (Database.get_n_most_recent_tips 5) *) in
+  lwt content = f a b in
   Lwt.return
     (html (head (title (pcdata "awesom's tips")) [])
        (body [h1 [pcdata "awesom's tips"];
               menu;
-              tips;
+              content;
               footer]))
 
-let empty_service_body _ _ =
-  Lwt.return
-    (html (head (title (pcdata "awesom's tips")) [])
-       (body [h1 [pcdata "TODO"]]))
-
 let services = [
-  (Services.main_service, main_service_body);
-  (Services.all_service, empty_service_body);
-  (Services.random_service, empty_service_body);
-  (Services.tags_service, empty_service_body);
-  (Services.add_service, empty_service_body);
-  (Services.rss_service, empty_service_body);
-  (Services.login_service, empty_service_body);
-  (Services.register_service, empty_service_body);
+  (Services.main_service, home_body);
+  (Services.all_service, todo_body);
+  (Services.random_service, todo_body);
+  (Services.tags_service, todo_body);
+  (Services.add_service, todo_body);
+  (Services.rss_service, todo_body);
+  (Services.login_service, todo_body);
+  (Services.register_service, Users.register_body);
 ]
 
 let _ =
   List.iter (fun (service, f) ->
     Eliom_registration.Html5.register
       ~service:service
-      f
+      (page f)
   ) services;
+  Eliom_registration.Html5.register
+    ~service:Services.register_confirm_service
+    (page Users.register_confirm);
+
   Eliom_registration.Action.register
     ~service:Services.logout_service
     (fun _ _ -> Eliom_state.discard ~scope:Eliom_common.default_session_scope ());
-
-
--- users.ml
+++ users.ml
@@ -1,7 +1,10 @@
 open Eliom_content.Html5.D
 open Eliom_parameter
 
-(* TODO: use a cookie for the username? *)
+(* TODO:
+  - login service
+  - register service
+  - use a cookie for the username? *)
 let username =
   Eliom_reference.eref ~scope:Eliom_common.default_session_scope
     (None : string option)
@@ -23,7 +26,46 @@ let connect_box () =
     (match user with
     | Some name -> div [p [pcdata ("connected as" ^ (if admin then "@" else ""))];
                         disconnect_box ()]
-    | None -> p [a ~service:Services.login_service [pcdata "login"] ();
-                 pcdata " or ";
-                 a ~service:Services.register_service [pcdata "register"] ()])
+    | None -> div [p [a ~service:Services.login_service [pcdata "login"] ();
+                      pcdata " or ";
+                      a ~service:Services.register_service [pcdata "register"] ()]])
+
+let register_form () =
+  post_form ~service:Services.register_confirm_service
+    (fun (username, (password1, password2)) ->
+      [fieldset
+         [label ~a:[a_for username] [pcdata "username: "];
+          string_input ~input_type:`Text ~name:username ();
+          br ();
+          label ~a:[a_for password1] [pcdata "password: "];
+          string_input ~input_type:`Password ~name:password1 ();
+          br ();
+          label ~a:[a_for password2] [pcdata "password (again): "];
+          string_input ~input_type:`Password ~name:password2 ();
+          br ();
+          string_input ~input_type:`Submit ~value:"Register" ()
+        ]])
+
+let register_body _ _ =
+  Lwt.return (register_form () ())
+
+let register_confirm () (name, (password1, password2)) =
+  let err =
+    if (password1 = password2) then
+      try
+        let user = {Data.name=name; Data.hash=Some (Sha256.string password1)} in
+        Data.add_user user;
+        None
+      with
+        (* TODO: more precise errors *)
+      | e -> Some "Error when adding the user (another user with the same username probably already exists)"
+    else
+      Some "Passwords don't match"
+  in
+  Lwt.return (match err with
+  | Some msg -> (p [pcdata ("Can't add user: " ^ msg)])
+  | None -> (p [pcdata "User successfully created, you can now ";
+                a ~service:Services.login_service [pcdata "log in"] ()]))
+
+
 
